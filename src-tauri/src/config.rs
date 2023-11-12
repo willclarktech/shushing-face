092121fd434b0469d3931cee2eab9f1879e0199b
willclarktech <willclarktech@users.noreply.github.com>
2023-11-13 00:06:29 +0100

    Add comments with iCloud/Dropbox locations

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -1,3 +1,7 @@
 pub const YO_DIRNAME: &str = ".yo";
 pub const TASKS_FILENAME: &str = "tasks";
 pub const SALT_FILENAME: &str = "tasks.salt";
+
+// iCloud drive default location: "~/Library/Mobile Documents/com~apple~CloudDocs/"
+// Dropbox config: ~/.dropbox/info.json
+// Dropbox default location: ~/Library/CloudStorage/Dropbox
