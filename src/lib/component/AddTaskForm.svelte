57805c448dabe6801c19783e8663dabe4f117e6d
willclarktech <willclarktech@users.noreply.github.com>
2023-11-12 23:52:05 +0100

    Put AddTaskForm in a modal

--- src/lib/component/AddTaskForm.svelte
+++ src/lib/component/AddTaskForm.svelte
@@ -9,7 +9,14 @@
 	let newDescription = "";
 	let newDetails = "";
 	let newDeadline = new Date().toDateString();
-	let showForm = false;
+	let isOpen = false;
+
+	const open = () => {
+		isOpen = true;
+	};
+	const close = () => {
+		isOpen = false;
+	};
 
 	const submit = () => {
 		void addTask(newDescription, newDeadline, newDetails);
@@ -20,57 +27,40 @@
 </script>
 
 <div class="container-narrow">
-	{#if showForm}
-		<form on:submit|preventDefault={submit}>
-			<fieldset>
-				<input
-					id="description"
-					name="description"
-					type="text"
-					placeholder="What needs to be done?"
-					bind:value={newDescription}
-				/>
-				<input
-					id="deadline"
-					name="deadline"
-					type="date"
-					bind:value={newDeadline}
-				/>
-				<details>
-					<summary>Details</summary>
-					<textarea
-						id="details"
-						name="details"
-						rows="5"
-						placeholder="Details"
-						bind:value={newDetails}
+	<button on:click={open}>Add a task</button>
+	<dialog open={isOpen}>
+		<div class="container-narrow">
+			<form on:submit|preventDefault={submit}>
+				<fieldset>
+					<input
+						id="description"
+						name="description"
+						type="text"
+						placeholder="What needs to be done?"
+						bind:value={newDescription}
 					/>
-				</details>
-				<div class="grid">
-					<button type="submit">
-						<Icon variant="plus" />
-						Add task
-					</button>
-					<button
-						class="secondary"
-						on:click={() => {
-							showForm = false;
-						}}
-					>
-						<Icon variant="times" />
-						Cancel
-					</button>
-				</div>
-			</fieldset>
-		</form>
-	{:else}
-		<button
-			on:click={() => {
-				showForm = true;
-			}}
-		>
-			<Icon variant="plus" />
-			Add a task
-		</button>
-	{/if}
+					<input
+						id="deadline"
+						name="deadline"
+						type="date"
+						bind:value={newDeadline}
+					/>
+					<details>
+						<summary>Details</summary>
+						<textarea
+							id="details"
+							name="details"
+							rows="5"
+							placeholder="Details"
+							bind:value={newDetails}
+						/>
+					</details>
+					<div class="grid">
+						<button type="submit">Save</button>
+						<button class="secondary" on:click={close}>Cancel</button>
+					</div>
+				</fieldset>
+			</form>
+		</div>
+	</dialog>
 </div>
