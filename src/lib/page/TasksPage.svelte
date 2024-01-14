c8e856abbad43a033dcc9a6efb43ea6f8037e608
willclarktech <willclarktech@users.noreply.github.com>
2024-01-14 19:40:42 +0100

    Rename AddTaskForm -> AddTaskButton

--- src/lib/page/TasksPage.svelte
+++ src/lib/page/TasksPage.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { Task } from "$lib/model";
-	import AddTaskForm from "$lib/component/AddTaskForm.svelte";
+	import AddTaskButton from "$lib/component/AddTaskButton.svelte";
 	import TaskList from "$lib/component/TaskList.svelte";
 
 	export let addTask: (task: Task) => Promise<void>;
@@ -13,7 +13,7 @@
 
 <div class="container">
 	<section>
-		<AddTaskForm {addTask} />
+		<AddTaskButton {addTask} />
 	</section>
 	<section>
 		<TaskList {tasks} {completeTask} {uncompleteTask} {editTask} {deleteTask} />
