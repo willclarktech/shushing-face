08f73a6318de0c9145d8d8690d5df8ef03919ddc
willclarktech <willclarktech@users.noreply.github.com>
2024-01-16 20:43:09 +0100

    Switch ChangePassword form to svelte-forms-lib and yup

--- src/lib/component/ChangePasswordForm.svelte
+++ src/lib/component/ChangePasswordForm.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { createForm } from "svelte-forms-lib";
+	import * as yup from "yup";
+
 	import { MINIMUM_PASSWORD_LENGTH } from "$lib/model";
 	import PasswordInput from "./PasswordInput.svelte";
 
@@ -9,120 +12,85 @@
 	) => void | Promise<void>;
 	export let visitTasks: () => void | Promise<void>;
 
-	let currentPasswordValue: string = "";
-	let newPasswordValue: string = "";
-	let repeatPasswordValue: string = "";
-	let submissionAttempted = false;
-	let errorMessage: string | null = null;
-	let currentPasswordError: string | null = null;
-	let newPasswordError: string | null = null;
-	let repeatPasswordError: string | null = null;
+	let submissionError: string | null = null;
 
-	const incorrectPasswordMessage = "Incorrect password";
+	const validationSchema = yup.object({
+		currentPassword: yup.string().required("Current password is required."),
+		newPassword: yup
+			.string()
+			.min(
+				MINIMUM_PASSWORD_LENGTH,
+				`New password must be at least ${MINIMUM_PASSWORD_LENGTH} characters long.`
+			)
+			.notOneOf(
+				[yup.ref("currentPassword")],
+				"New password must not be the same as current password."
+			),
+		repeatPassword: yup
+			.string()
+			.oneOf([yup.ref("newPassword")], "Passwords do not match."),
+	});
 
-	const clearValidation = () => {
-		submissionAttempted = false;
-		currentPasswordError = null;
-		newPasswordError = null;
-		repeatPasswordError = null;
-		errorMessage = null;
-	};
-
-	const validateForm = () => {
-		let isValid = true;
-
-		if (currentPasswordValue.length === 0) {
-			currentPasswordError = "Current password is required.";
-			isValid = false;
-		} else {
-			currentPasswordError = null;
-		}
-
-		if (newPasswordValue.length < MINIMUM_PASSWORD_LENGTH) {
-			newPasswordError = `New password must be at least ${MINIMUM_PASSWORD_LENGTH} characters long.`;
-			isValid = false;
-		} else if (newPasswordValue === currentPasswordValue) {
-			newPasswordError =
-				"New password must not be the same as current password.";
-			isValid = false;
-		} else {
-			newPasswordError = null;
-		}
-
-		if (repeatPasswordValue !== newPasswordValue) {
-			repeatPasswordError = "Passwords do not match.";
-			isValid = false;
-		} else {
-			repeatPasswordError = null;
-		}
-
-		return isValid;
-	};
-
-	const submit = async () => {
-		submissionAttempted = true;
-		const isFormValid = validateForm();
-		if (!isFormValid) {
-			return;
-		}
-
-		try {
-			await changePassword(
-				currentPasswordValue,
-				newPasswordValue,
-				repeatPasswordValue
-			);
-			visitTasks();
-		} catch (error) {
-			if (/incorrect password/i.test(error as string)) {
-				currentPasswordError = incorrectPasswordMessage;
-			} else {
-				errorMessage = "Failed to change password. Please try again.";
+	const { form, errors, handleSubmit } = createForm({
+		initialValues: {
+			currentPassword: "",
+			newPassword: "",
+			repeatPassword: "",
+		},
+		validationSchema,
+		onSubmit: async (values) => {
+			try {
+				await changePassword(
+					values.currentPassword,
+					values.newPassword,
+					values.repeatPassword
+				);
+				visitTasks();
+			} catch (error) {
+				if (/incorrect password/i.test(error as string)) {
+					errors.update((e) => ({
+						...e,
+						currentPassword: "Incorrect password.",
+					}));
+				} else {
+					submissionError = "Failed to change password. Please try again.";
+				}
 			}
-		}
-	};
+		},
+	});
 </script>
 
 <div class="container-narrow">
 	<div class="container-vertical">
-		<form on:submit|preventDefault={submit}>
+		<form on:submit|preventDefault={handleSubmit}>
 			<fieldset>
 				<PasswordInput
 					id="current"
 					placeholder="Current password"
-					bind:value={currentPasswordValue}
-					invalid={typeof currentPasswordError === "string"
-						? true
-						: currentPasswordError}
-					errorMessage={currentPasswordError}
-					onInput={clearValidation}
+					bind:value={$form.currentPassword}
+					invalid={!!$errors.currentPassword}
+					errorMessage={$errors.currentPassword}
 				/>
 				<PasswordInput
 					id="new"
 					placeholder="New password"
-					bind:value={newPasswordValue}
-					invalid={typeof newPasswordError === "string"
-						? true
-						: newPasswordError}
-					errorMessage={newPasswordError}
-					onInput={clearValidation}
+					bind:value={$form.newPassword}
+					invalid={!!$errors.newPassword}
+					errorMessage={$errors.newPassword}
 				/>
 				<PasswordInput
 					id="repeat"
 					placeholder="Repeat password"
-					bind:value={repeatPasswordValue}
-					invalid={typeof repeatPasswordError === "string"
-						? true
-						: repeatPasswordError}
-					errorMessage={repeatPasswordError}
-					onInput={clearValidation}
+					bind:value={$form.repeatPassword}
+					invalid={!!$errors.repeatPassword}
+					errorMessage={$errors.repeatPassword}
 				/>
-				{#if submissionAttempted && errorMessage}
-					<small class="error-message">{errorMessage}</small>
+				{#if submissionError}
+					<small class="error-message">{submissionError}</small>
 				{/if}
 				<div class="grid">
-					<button type="submit"> Change password </button>
-					<button class="secondary" on:click={visitTasks}> Cancel </button>
+					<button type="submit">Change password</button>
+					<button class="secondary" on:click={visitTasks}>Cancel</button>
 				</div>
 			</fieldset>
 		</form>
