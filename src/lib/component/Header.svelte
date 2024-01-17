0388742624c81991aa38b6d708c2404cbceba6b7
willclarktech <willclarktech@users.noreply.github.com>
2024-01-17 10:12:48 +0100

    Redesign header/settings menu

--- src/lib/component/Header.svelte
+++ src/lib/component/Header.svelte
@@ -1,27 +1,41 @@
 <script lang="ts">
 	import logo from "$lib/assets/Square71x71Logo.png";
-	import { Page } from "$lib/model";
-	import Settings from "./Settings.svelte";
+	import Icon from "$lib/component/Icon.svelte";
 
-	export let page: Page;
 	export let lock: () => void | Promise<void>;
 	export let visitChangeSettings: () => void | Promise<void>;
 	export let visitChangePassword: () => void | Promise<void>;
 </script>
 
-{#if ![Page.Loading, Page.Unlock].includes(page)}
-	<header class="container-fluid">
-		<nav>
-			<ul>
-				<li>
-					<img src={logo} alt="logo" />
-				</li>
-			</ul>
-			<ul>
-				<li>
-					<Settings {lock} {visitChangeSettings} {visitChangePassword} />
-				</li>
-			</ul>
-		</nav>
-	</header>
-{/if}
+<header class="container-fluid">
+	<nav>
+		<ul>
+			<li>
+				<img src={logo} alt="logo" />
+			</li>
+		</ul>
+		<ul>
+			<li>
+				<button
+					type="button"
+					on:click={() => {
+						visitChangePassword();
+						close();
+					}}
+				>
+					<Icon variant="key" />
+				</button>
+				<button
+					type="button"
+					on:click={() => {
+						visitChangeSettings();
+						close();
+					}}
+				>
+					<Icon variant="cog" />
+				</button>
+				<button type="button" on:click={lock}><Icon variant="lock" /></button>
+			</li>
+		</ul>
+	</nav>
+</header>
