72e8c87b9d6d5325ffab6a4a4eb16412a38468ed
willclarktech <willclarktech@users.noreply.github.com>
2024-01-18 21:13:50 +0100

    Update PasswordInput for svelte-forms-lib and Pico v2

--- src/lib/component/PasswordInput.svelte
+++ src/lib/component/PasswordInput.svelte
@@ -1,68 +1,46 @@
 <script lang="ts">
+	import { ErrorMessage, Field } from "svelte-forms-lib";
+
 	import Icon from "$lib/component/Icon.svelte";
 
 	export let id: string;
-	export let placeholder: string;
-	export let value: string;
+	export let label: string;
+	export let placeholder: string = "";
 	export let invalid: boolean | null = null;
-	export let errorMessage: string | null = null;
-	export let onInput: undefined | (() => void | Promise<void>) = undefined;
+	export let helpText: string = "";
 
-	let inputElement: HTMLInputElement;
 	let showPassword = false;
 
 	const togglePasswordVisibility = () => {
 		showPassword = !showPassword;
-		// NOTE: Can't bind input type
-		inputElement.type = showPassword ? "text" : "password";
 	};
 
+	$: inputType = showPassword ? "text" : "password";
 	$: variant = showPassword ? "eyeHide" : "eyeShow";
-	$: errorId = `${id}-error`;
 </script>
 
 <div>
-	{#if errorMessage}
-		<small id={errorId} class="error-message">{errorMessage}</small>
-	{/if}
-	<div class="input-group">
-		<input
-			bind:this={inputElement}
-			{id}
-			name={id}
-			type="password"
-			{placeholder}
-			bind:value
-			autocomplete="off"
-			autocapitalize="off"
-			autocorrect="off"
-			required
-			on:input={onInput}
-			aria-invalid={invalid}
-			aria-errormessage={errorMessage ? errorId : undefined}
-		/>
-		<button
-			type="button"
-			class="toggle-visibility"
-			on:click={togglePasswordVisibility}
-		>
-			<Icon {variant} />
-		</button>
+	<div class="grid" aria-invalid={invalid}>
+		<fieldset role="group">
+			<Field
+				{id}
+				name={id}
+				type={inputType}
+				{label}
+				{placeholder}
+				autocomplete="off"
+				autocapitalize="off"
+				autocorrect="off"
+				required
+				aria-invalid={invalid}
+			/>
+			<button type="button" on:click={togglePasswordVisibility}>
+				<Icon {variant} />
+			</button>
+		</fieldset>
 	</div>
+	<ErrorMessage name={id} />
+	{#if helpText}
+		<small>{helpText}</small>
+	{/if}
 </div>
-
-<style>
-	.input-group {
-		display: flex;
-	}
-
-	.toggle-visibility {
-		max-width: 3rem;
-		right: 0;
-		align-self: center;
-	}
-
-	.error-message {
-		color: red;
-	}
-</style>
